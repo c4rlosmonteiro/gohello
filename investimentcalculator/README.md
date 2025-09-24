7b193ef0e87d4de3659bc7ef1517c3a0afddf0dd
Carlos Monteiro <carlosem4il@gmail.com>
2025-09-23 23:52:37 -0300

    investment calculator

--- investimentcalculator/README.md
+++ investimentcalculator/README.md
@@ -1,3 +1,11 @@
 
 You can run this application by executing the following command in your terminal:
 `./investment-calculator`
+
+For developer:
+
+Build: 
+`go build .`
+
+Run:
+`go run  .`
--- investimentcalculator/investiment_calculator.go
+++ investimentcalculator/investiment_calculator.go
@@ -1,14 +1,36 @@
 package main
 
-import "fmt"
-import "math"
+import (
+	"fmt"
+	"math"
+)
+
+func info() {
+	//const
+
+	const inflationRate1 = 2.5
+	//or
+	const inflationRate2 float64 = 2
+
+	//var
+
+}
 
 func main() {
+	const inflationRate = 6.5
+	//This varibale will be assigned its default "null value" automatically
+	//by Go. In case of a float64 value, tha's "0.0"
+	var investmentAmount float64
+	years := 10.0
+	expectedReturnRate := 5.5
 
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	//To use scan you need to pass a pointer
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
 
-	var fetutureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
